b1278df250704005e6edde4f7aecc8ec7d3b08f4
moevegas <moevegas@gmail.com>
2017-05-14 16:12:48 -0400

    Add project files.

---
+++ Map01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Map01.MvcApplication" Language="C#" %>
---
+++ Map01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Map01.Startup))]
+namespace Map01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
